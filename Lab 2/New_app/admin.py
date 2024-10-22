6d4694bc0a455fa85ef3a2c5d347e8fba7b02ce3
Назар Кулішов <158081111+soulofalquimista@users.noreply.github.com>
2024-10-22 21:17:26 +0300

    Delete Lab 2 directory

--- Lab 2/New_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from new_app.models import news
-admin.site.register(news)
--- Lab 2/New_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-python manage.py startapp blog
-
-
-class NewAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'new_app'
